6ea61ddebd0f9e76d1939c2efde3f215f0c1a89b
손량 <loop.infinitely@gmail.com>
2019-04-04 14:37:17 +0900

    added crude basketball court

--- basketball_gym/envs/basketball.py
+++ basketball_gym/envs/basketball.py
@@ -15,6 +15,17 @@ class BasketballEnv(gym.Env):
 
     def __init__(self):
         self.viewer = None
+        self.player_pos = (125, 80)
+        self.enemy_pos = (125, 320)
+        self.PLAYER_SIZE = 16
+
+    def rect_coords(self, pos):
+        return [
+            (pos[0] - self.PLAYER_SIZE / 2, pos[1] - self.PLAYER_SIZE / 2),
+            (pos[0] + self.PLAYER_SIZE / 2, pos[1] - self.PLAYER_SIZE / 2),
+            (pos[0] + self.PLAYER_SIZE / 2, pos[1] + self.PLAYER_SIZE / 2),
+            (pos[0] - self.PLAYER_SIZE / 2, pos[1] + self.PLAYER_SIZE / 2),
+        ]
 
     def step(self, action):
         pass
@@ -28,8 +39,30 @@ class BasketballEnv(gym.Env):
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            rect = rendering.FilledPolygon([(0, 0), (250, 0), (0, 400)])
-            self.viewer.add_geom(rect)
+            upper_goal = rendering.FilledPolygon([
+                (110, 0),
+                (140, 0),
+                (140, 30),
+                (110, 30),
+            ])
+            lower_goal = rendering.FilledPolygon([
+                (110, 400),
+                (140, 400),
+                (140, 370),
+                (110, 370),
+            ])
+            self.viewer.add_geom(upper_goal)
+            self.viewer.add_geom(lower_goal)
+            player = rendering.FilledPolygon(self.rect_coords(self.player_pos))
+            self.player_trans = rendering.Transform()
+            player.add_attr(self.player_trans)
+            player.set_color(0, 0.5, 0)
+            self.viewer.add_geom(player)
+            enemy = rendering.FilledPolygon(self.rect_coords(self.enemy_pos))
+            self.enemy_trans = rendering.Transform()
+            enemy.add_attr(self.enemy_trans)
+            enemy.set_color(1, 0, 0)
+            self.viewer.add_geom(enemy)
         return self.viewer.render(return_rgb_array=mode=='rgb_array')
 
     def close(self):
