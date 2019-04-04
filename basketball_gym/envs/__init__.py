cf1d82251de0cead22c18ccb0fabd9d0faf5a0f5
손량 <loop.infinitely@gmail.com>
2019-04-04 16:19:20 +0900

    added docstring

--- basketball_gym/envs/__init__.py
+++ basketball_gym/envs/__init__.py
@@ -1,1 +1,15 @@
+"""
+Basketball Gym
+==============
+
+A basketball simulation for RL algorithms
+
+Gyms
+----
+List of OpenAI gyms
+
+============= ===============================
+BasketballEnv A single player basketball game
+============= ===============================
+"""
 from basketball_gym.envs.basketball import BasketballEnv
